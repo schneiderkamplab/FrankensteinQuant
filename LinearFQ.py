feb20d3b513b2c192000b9027a414f9103a3a3ee
Jacob Nielsen <36268638+JacobBITLABS@users.noreply.github.com>
2025-10-13 15:39:40 +0200

    remove bit choices

--- LinearFQ.py
+++ LinearFQ.py
@@ -7,12 +7,12 @@ from gumbel_bit_quantizer import GumbelBitQuantizer
 class LinearFQ(nn.Module):
     def __init__(
         self,
-            in_features,
-            out_features,
-            bias=True,
-            device=None,
-            dtype=None,
-            name="fc",
+        in_features,
+        out_features,
+        bias=True,
+        device=None,
+        dtype=None,
+        name="fc",
     ):
         super(LinearFQ, self).__init__(
             in_features=in_features,
@@ -21,8 +21,6 @@ class LinearFQ(nn.Module):
             device=device,
             dtype=dtype,
         )
-        self.bit_choices = [2, 4, 8]
-        self.K = len(self.bit_choices)
         self.COST_TABLE = {2: 0.5, 4: 1.0, 8: 2.0, 16: 3.0}  # example proxy cost        
         self.w_q = GumbelBitQuantizer(name=f"{name}_w")
         self.a_q = GumbelBitQuantizer(name=f"{name}_a")
