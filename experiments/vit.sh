e3c0fc6d893015e0f59aec0acfef6228b0a642fc
Jacob Nielsen <36268638+JacobBITLABS@users.noreply.github.com>
2026-01-09 09:04:59 +0100

    t5

---
+++ experiments/vit.sh
@@ -0,0 +1,7 @@
+python3 main.py --model-type vit --lr 0.001 --epochs 5 --log
+
+python3 main.py --model-type vit --lr 0.001 --epochs 5 --use-quant --bit-choices "[8, 16]" --log
+
+python3 main.py --model-type vit --lr 0.001 --epochs 5 --use-quant --bit-choices "[4, 8, 16]" --log
+
+python3 main.py --model-type vit --lr 0.001 --epochs 5 --use-quant --bit-choices "[2, 4, 8, 16]" --log 
